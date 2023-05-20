5d1da5ae39ed6b458292f017b591eba65463c023
vMiksta <miksta100@live.com.au>
2023-05-20 23:03:58 +1000

    Updated Userhomepage

--- app/src/main/java/com/example/bookit/Userhomepage.java
+++ app/src/main/java/com/example/bookit/Userhomepage.java
@@ -1,70 +1,127 @@
 package com.example.bookit;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class Userhomepage extends AppCompatActivity implements View.OnClickListener {
+// implements View.OnClickListener  ***Previously added
+public class Userhomepage extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userhomepage_activity);
 
-        ImageView hairdresserImage = findViewById(R.id.Hairdresser);
-        ImageView chiropractorImage = findViewById(R.id.Chiropractor);
-        ImageView restaurantImage = findViewById(R.id.Dentist);
-        ImageView physiotherapistImage = findViewById(R.id.Physiotherapist);
+        ////******CHANGES FROM HERE***
 
-        hairdresserImage.setOnClickListener(this);
-        chiropractorImage.setOnClickListener(this);
-        restaurantImage.setOnClickListener(this);
-        physiotherapistImage.setOnClickListener(this);
-    }
+        Button chiropractorButton = findViewById(R.id.Chiropractor);
+        chiropractorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectChiropractor();
+            }
+        });
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.Hairdresser:
-                selectHairdresser();
-                break;
-            case R.id.Chiropractor:
-                selectMechanic();
-                break;
-            case R.id.Dentist:
-                selectRestaurant();
-                break;
-            case R.id.Physiotherapist:
+        Button physiotherapistButton = findViewById(R.id.Physiotherapist);
+        physiotherapistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 selectPhysiotherapist();
-                break;
-        }
+            }
+        });
+
+        Button dentistButton = findViewById(R.id.Dentist);
+        dentistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDentist();
+            }
+        });
     }
 
     public void selectHairdresser() {
-        // Code to handle hairdresser selection
-        // You can instantiate DisplayBusiness and pass the selected type as an argument
-        DisplayBusiness displayBusiness = new DisplayBusiness();
-        displayBusiness.fetchBusinessesWithType("Hairdresser");
+        Intent intent = new Intent(this, DisplayBusiness.class);
+        intent.putExtra("type", "Hairdresser");
+        startActivity(intent);
     }
 
-    public void selectMechanic() {
-        // Code to handle mechanic selection
-        // You can instantiate DisplayBusiness and pass the selected type as an argument
-        DisplayBusiness displayBusiness = new DisplayBusiness();
-        displayBusiness.fetchBusinessesWithType("Chiropractor");
-    }
-
-    public void selectRestaurant() {
-        // Code to handle restaurant selection
-        // You can instantiate DisplayBusiness and pass the selected type as an argument
-        DisplayBusiness displayBusiness = new DisplayBusiness();
-        displayBusiness.fetchBusinessesWithType("Physiotherapy");
+    public void selectChiropractor() {
+        Intent intent = new Intent(this, DisplayBusiness.class);
+        intent.putExtra("type", "Chiropractor");
+        startActivity(intent);
     }
 
     public void selectPhysiotherapist() {
-        // Code to handle physiotherapist selection
-        // You can instantiate DisplayBusiness and pass the selected type as an argument
-        DisplayBusiness displayBusiness = new DisplayBusiness();
-        displayBusiness.fetchBusinessesWithType("Dentist");
+        Intent intent = new Intent(this, DisplayBusiness.class);
+        intent.putExtra("type", "Physiotherapist");
+        startActivity(intent);
+    }
+
+    public void selectDentist() {
+        Intent intent = new Intent(this, DisplayBusiness.class);
+        intent.putExtra("type", "Dentist");
+        startActivity(intent);
     }
 }
+
+
+//
+//        ImageView hairdresserImage = findViewById(R.id.Hairdresser);
+//        ImageView chiropractorImage = findViewById(R.id.Chiropractor);
+//        ImageView restaurantImage = findViewById(R.id.Dentist);
+//        ImageView physiotherapistImage = findViewById(R.id.Physiotherapist);
+//
+//        hairdresserImage.setOnClickListener(this);
+//        chiropractorImage.setOnClickListener(this);
+//        restaurantImage.setOnClickListener(this);
+//        physiotherapistImage.setOnClickListener(this);
+//    }
+//
+//    public void selectHairdresser() {
+//        // Code to handle hairdresser selection
+//        // You can instantiate DisplayBusiness and pass the selected type as an argument
+//        DisplayBusiness displayBusiness = new DisplayBusiness();
+//        displayBusiness.fetchBusinessesWithType("Hairdresser");
+//    }
+//
+//    public void selectMechanic() {
+//        // Code to handle mechanic selection
+//        // You can instantiate DisplayBusiness and pass the selected type as an argument
+//        DisplayBusiness displayBusiness = new DisplayBusiness();
+//        displayBusiness.fetchBusinessesWithType("Chiropractor");
+//    }
+//
+//    public void selectRestaurant() {
+//        // Code to handle restaurant selection
+//        // You can instantiate DisplayBusiness and pass the selected type as an argument
+//        DisplayBusiness displayBusiness = new DisplayBusiness();
+//        displayBusiness.fetchBusinessesWithType("Physiotherapy");
+//    }
+//
+//    public void selectPhysiotherapist() {
+//        // Code to handle physiotherapist selection
+//        // You can instantiate DisplayBusiness and pass the selected type as an argument
+//        DisplayBusiness displayBusiness = new DisplayBusiness();
+//        displayBusiness.fetchBusinessesWithType("Dentist");
+//    }
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.Hairdresser:
+//                selectHairdresser();
+//                break;
+//            case R.id.Chiropractor:
+//                selectMechanic();
+//                break;
+//            case R.id.Dentist:
+//                selectRestaurant();
+//                break;
+//            case R.id.Physiotherapist:
+//                selectPhysiotherapist();
+//                break;
+//        }
+//    }
+
+//}
